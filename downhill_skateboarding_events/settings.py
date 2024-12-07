be16c35e043081df9b5af31a34b03e1974ebe6d8
pearsedarcy <pearse.darcy@outlook.com>
2024-12-07 19:10:53 +0100

    Feat: Add a styled signup form template

--- downhill_skateboarding_events/settings.py
+++ downhill_skateboarding_events/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = 'downhill_skateboarding_events.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
