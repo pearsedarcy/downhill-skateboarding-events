2e19ade5c560e0b9ed7179b683c696de2376b445
pearsedarcy <pearse.darcy@outlook.com>
2024-12-10 14:45:08 +0100

    Feat: Add Cloudinary and django-countries support in settings

--- downhill_skateboarding_events/settings.py
+++ downhill_skateboarding_events/settings.py
@@ -48,6 +48,9 @@ INSTALLED_APPS = [
     "tailwind",
     "theme",
     "django_browser_reload",
+    "django_countries",
+    "cloudinary",
+    "cloudinary_storage",
 ]
 
 
@@ -153,7 +156,21 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_DIRS = [BASE_DIR / "theme/static", BASE_DIR / "static"]
+STATICFILES_DIRS = [
+    BASE_DIR / "theme/static",
+    BASE_DIR / "static",
+]
+
+CLOUDINARY_STORAGE = {
+    "CLOUD_NAME": env("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": env("CLOUDINARY_API_KEY"),
+    "API_SECRET": env("CLOUDINARY_API_SECRET"),
+}
+
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
